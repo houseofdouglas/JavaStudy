022e08a8ca4add49ca7dcaef9394d44febc531be
Peter Douglas <peter@thehouseofdouglas.com>
2015-06-13 11:22:57 -0600

    include tutorial 1, add tutorial 2

---
+++ OSGi/src/main/java/tutorial/example2/Activator.java
@@ -0,0 +1,13 @@
+public tutorial.example2;
+
+import java.util.Hashtable;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceListener;
+import org.osgi.framework.ServiceEvent;
+
+public class Activator implements BundleActivator
+{
+    
+}
---
+++ OSGi/src/main/java/tutorial/example2/service/DictionaryService.java
@@ -0,0 +1,6 @@
+package tutorial.example.service;
+
+public interface DictionaryService
+{
+    public boolean checkWord(String word);
+}
